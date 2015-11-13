129377d0b21bb60e0f45b5544f78e6fb120ff443
kerwinkerwin <kerwin@enspiral.com>
2015-11-13 15:01:23 +1300

    intial commit

--- js/contact_me.js
+++ js/contact_me.js
@@ -12,22 +12,24 @@ $(function() {
 
             // get values from FORM
             var name = $("input#name").val();
+            var age = $("input#age").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
+            var highschool = $("input#highschool").val();
+            var gender = $("input#gender").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://zapier.com/hooks/catch/3kpxrl/",
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
+                    age: age,
                     email: email,
-                    message: message
+                    highschool: highschool,
+                    gender: gender
                 },
                 cache: false,
                 success: function() {
@@ -37,7 +39,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Registration submitted. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -49,7 +51,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems there was an error. Please contact me at kerwin@devacademy.co.nz!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
